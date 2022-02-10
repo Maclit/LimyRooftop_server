901b5a285afd063ecc58cb98d2796c06284853d4
unknown <julien.castillejos@epitech.eu>
2022-02-10 14:01:21 +0100

    Basic server

---
+++ server/src/main.rs
@@ -0,0 +1,13 @@
+use actix_web::{App, HttpServer };
+mod user;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(user::register)
+    })
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
+}
---
+++ server/src/user.rs
@@ -0,0 +1,16 @@
+use actix_web::{ post, get, HttpResponse };
+
+#[post("/api/users/v1/register")]
+pub async fn register() -> HttpResponse {
+    HttpResponse::Ok().body("OK")
+}
+
+#[post("/api/users/v1/login")]
+pub async fn login() -> HttpResponse {
+    HttpResponse::Ok().body("OK")
+}
+
+#[get("/api/users/v1/me")]
+pub async fn user_info() -> HttpResponse {
+    HttpResponse::Ok().body("OK")
+}
